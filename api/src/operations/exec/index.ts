4492d9c7bbf60202bb3b3f464376259f73890806
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-07-27 14:23:43 +0200

    replace vm2 with `isolated-vm`
    this doesnt include module support (yet)

--- api/src/operations/exec/index.ts
+++ api/src/operations/exec/index.ts
@@ -1,7 +1,8 @@
 import { defineOperationApi, toArray } from '@directus/utils';
-import { isBuiltin } from 'node:module';
-import type { NodeVMOptions } from 'vm2';
-import { NodeVM, VMScript } from 'vm2';
+import { isBuiltin, createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+const ivm = require('isolated-vm');
 
 type Options = {
 	code: string;
@@ -15,12 +16,7 @@ export default defineOperationApi<Options>({
 		const allowedModulesExternal: string[] = [];
 		const allowedEnv = data['$env'] ?? {};
 
-		const opts: NodeVMOptions = {
-			eval: false,
-			wasm: false,
-			env: allowedEnv,
-		};
-
+		// TODO: Check if we can make dependencies work
 		for (const module of allowedModules) {
 			if (isBuiltin(module)) {
 				allowedModulesBuiltIn.push(module);
@@ -29,21 +25,38 @@ export default defineOperationApi<Options>({
 			}
 		}
 
-		if (allowedModules.length > 0) {
-			opts.require = {
-				builtin: allowedModulesBuiltIn,
-				external: {
-					modules: allowedModulesExternal,
-					transitive: false,
-				},
-			};
-		}
+		const isolateSizeMb = 64; // Arbitrary choice for testing
+		const scriptTimeoutMs = 10000; // Arbitrary choice for testing
+		const isolate = new ivm.Isolate({ memoryLimit: isolateSizeMb });
 
-		const vm = new NodeVM(opts);
+		const context = isolate.createContextSync();
+		const jail = context.global;
+		jail.setSync('global', jail.derefInto());
 
-		const script = new VMScript(code).compile();
-		const fn = await vm.run(script);
+		// We will create a basic `log` function for the new isolate to use.
+		// TODO: This is just for testing, else the logs get swallowed inside the isolate.
+		jail.setSync('log', function (...args) {
+			console.log(...args);
+		});
+		jail.setSync('process', { env: allowedEnv }, { copy: true });
+		jail.setSync('module', { exports: null }, { copy: true });
 
-		return await fn(data);
+		// We run the operation once to define the module.exports function
+		const hostile = isolate.compileScriptSync(code);
+		await hostile.run(context, { timeout: scriptTimeoutMs }).catch((err: any) => { console.error(err); throw err; })
+
+		const inputData = new ivm.Reference({ data });
+		// TODO: Learn about references inside the sandbox
+		const resultRef = await context.evalClosure(
+			`return module.exports($0.copySync().data)`, 
+			[inputData],
+			{ result: { reference: true, promise: true },  timeout: scriptTimeoutMs }
+		).catch((err: any) => { 
+			console.error(err)
+			throw err;
+		});
+		
+		// TODO: releasing the resources would be nice, lookup in which order
+		return resultRef.copySync();
 	},
 });
